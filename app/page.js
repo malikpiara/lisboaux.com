6c219809116646723fb3a9f42ea6ba17fb0b962d
Malik Piara <malik@hey.com>
2025-03-08 05:43:39 +0000

    First version of the website.

--- app/page.js
+++ app/page.js
@@ -1,100 +1,51 @@
-import Image from "next/image";
+import Link from "next/link"
+import { inter } from "./fonts"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className={`flex min-h-screen flex-col ${inter.className}`}>
+      <header className="text-lg">
+        <div className="mx-auto flex h-14 max-w-[1024px] items-center justify-between px-4">
+          <div className="font- font-extrabold">
+            <Link href="/">LisboaUX</Link>
+          </div>
+          <nav className="flex gap-5 text-base">
+            <Link href="https://lu.ma/lisboaux" className="hover:text-primary hover:opacity-30 transition-all">
+              Events
+            </Link>
+            <Link href="https://lisboaux.com/slack" className="hover:text-primary hover:opacity-30 transition-all">
+              Community
+            </Link>
+            <Link href="https://www.linkedin.com/company/lisboaux/" className="hover:text-primary hover:opacity-30 transition-all">
+              LinkedIn
+            </Link>
+            <Link href='https://www.youtube.com/@LisboaUX' className="hover:text-primary hover:opacity-30 transition-all">Youtube</Link>
+          </nav>
         </div>
+      </header>
+
+      <main className="flex-1 text-lg animate-in">
+        <section className="mx-auto max-w-[1024px] py-10 px-4">
+          <h1 className="text-4xl mx-auto text-muted-foreground scroll-m-20 tracking-tight lg:text-4xl">Welcome to LisboaUX</h1>
+          <p className="max-w-[624px] text-muted-foreground leading-7 [&:not(:first-child)]:mt-6">
+          We are a community of designers based in Lisboa. Every month, we host <span className="font-bold">talks</span> & <span className="font-bold">intimate dinners</span> to nurture growth and belonging. Come listen to our stories and hone your craft together with us.
+          </p>
+          <Link className="text-muted-foreground text-muted-foreground leading-7 block w-fit mt-6 border-b pb-1 hover:border-0 transition-all hover:opacity-30" href='https://lisboaux.com/slack'>Join our community on Slack.</Link>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="">
+        <div className="mx-auto flex max-w-[1024px] items-center justify-between px-4 py-4">
+          <p className="text-muted-foreground">© {new Date().getFullYear()} LisboaUX</p>
+          {/* <div className="flex gap-3 text-xs text-muted-foreground">
+            <Link href="/terms" className="hover:text-foreground">
+              Terms
+            </Link>
+            <Link href="/privacy" className="hover:text-foreground">
+              Privacy
+            </Link>
+          </div> */}
+        </div>
       </footer>
     </div>
   );
